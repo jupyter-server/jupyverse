5b06df8efd7a68ded8fdad097396807276706380
David Brochart <david.brochart@gmail.com>
2021-09-08 17:20:24 +0200

    Release 0.0.4

--- plugins/contents/setup.py
+++ plugins/contents/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages  # type: ignore
 
 setup(
     name="fps_contents",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     install_requires=["fps", "aiofiles", "fps-auth"],
     entry_points={"fps_router": ["fps-contents = fps_contents.routes"]},
