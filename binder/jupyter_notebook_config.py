68a266e73709cf8d65dabc85c2ce46420a5f846a
David Brochart <david.brochart@gmail.com>
2021-09-24 14:27:03 +0200

    Add RetroLab Binder

--- binder/jupyter_notebook_config.py
+++ binder/jupyter_notebook_config.py
@@ -1,24 +1,43 @@
-jupyverse_command = ' '.join([
+jupyverse_jlab_command = ' '.join([
     'jupyverse',
     '--no-open-browser',
     '--authenticator.mode=noauth',
+    '--RetroLab.enabled=false',
     '--JupyterLab.collaborative',
-    '--JupyterLab.base_url={base_url}jupyverse/',
+    '--JupyterLab.base_url={base_url}jupyverse-jlab/',
     '--port={port}',
-] + ['>jupyverse.log 2>&1'])
+] + ['>jupyverse_jlab.log 2>&1'])
+
+
+jupyverse_rlab_command = ' '.join([
+    'jupyverse',
+    '--no-open-browser',
+    '--authenticator.mode=noauth',
+    '--JupyterLab.enabled=false',
+    '--RetroLab.collaborative',
+    '--RetroLab.base_url={base_url}jupyverse-rlab/',
+    '--port={port}',
+] + ['>jupyverse_rlab.log 2>&1'])
 
 
 c.ServerProxy.servers = {
-    'jupyverse': {
+    'jupyverse-jlab': {
         'command': [
-            '/bin/bash', '-c', jupyverse_command
+            '/bin/bash', '-c', jupyverse_jlab_command
+        ],
+        'timeout': 60,
+        'absolute_url': False
+    },
+    'jupyverse-rlab': {
+        'command': [
+            '/bin/bash', '-c', jupyverse_rlab_command
         ],
         'timeout': 60,
         'absolute_url': False
     },
 }
 
-c.NotebookApp.default_url = '/jupyverse'
+c.NotebookApp.default_url = '/jupyverse-jlab'
 
 import logging
 c.NotebookApp.log_level = logging.DEBUG
