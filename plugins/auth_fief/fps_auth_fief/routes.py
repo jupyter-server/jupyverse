18b269085bf4907a07d47ab97f70cd5a5a2de7c9
David Brochart <david.brochart@gmail.com>
2022-08-26 11:58:06 +0200

    Add /api/me for Fief auth

--- plugins/auth_fief/fps_auth_fief/routes.py
+++ plugins/auth_fief/fps_auth_fief/routes.py
@@ -1,8 +1,12 @@
-from fastapi import APIRouter, Query, Request, Response
+from typing import Dict, List, Optional
+
+from fastapi import APIRouter, Depends, Query, Request, Response
 from fastapi.responses import RedirectResponse
+from fief_client import FiefAccessTokenInfo
 from fps.hooks import register_router
 
-from .backend import SESSION_COOKIE_NAME, fief
+from .backend import SESSION_COOKIE_NAME, auth, current_user, fief
+from .models import Permissions, UserRead
 
 router = APIRouter()
 
@@ -22,6 +26,36 @@ async def auth_callback(request: Request, response: Response, code: str = Query(
     )
 
     return response
+
+
+@router.get("/api/me")
+async def get_api_me(
+    permissions_to_check: Optional[Permissions] = None,
+    user: UserRead = Depends(current_user()),
+    access_token_info: FiefAccessTokenInfo = Depends(auth.authenticated()),
+):
+    checked_permissions: Dict[str, List[str]] = {}
+    if permissions_to_check is not None:
+        permissions = permissions_to_check.permissions
+        user_permissions = {}
+        for permission in access_token_info["permissions"]:
+            resource, action = permission.split(":")
+            if resource not in user_permissions.keys():
+                user_permissions[resource] = []
+            user_permissions[resource].append(action)
+        for resource, actions in permissions.items():
+            user_resource_permissions = user_permissions.get(resource, [])
+            allowed = checked_permissions[resource] = []
+            for action in actions:
+                if action in user_resource_permissions:
+                    allowed.append(action)
+
+    keys = ["username", "name", "display_name", "initials", "avatar_url", "color"]
+    identity = {k: getattr(user, k) for k in keys}
+    return {
+        "identity": identity,
+        "permissions": checked_permissions,
+    }
 
 
 r = register_router(router)
