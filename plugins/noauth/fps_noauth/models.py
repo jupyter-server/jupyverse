aa3439623acc2720d716e0ad6046b9207d5d7dcd
David Brochart <david.brochart@gmail.com>
2022-12-12 18:30:27 +0100

    Add resource_usage plugin (#253)

---
+++ plugins/noauth/fps_noauth/models.py
@@ -0,0 +1,15 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+    anonymous: bool = True
+    username: str = ""
+    name: str = ""
+    display_name: str = ""
+    initials: Optional[str] = None
+    color: Optional[str] = None
+    avatar_url: Optional[str] = None
+    workspace: str = "{}"
+    settings: str = "{}"
