09870a3de4d1b688ae59ee41b63860409a3700a9
David Brochart <david.brochart@gmail.com>
2023-01-06 17:32:50 +0100

    Pass environment to terminals (#261)

--- plugins/terminals/fps_terminals/server.py
+++ plugins/terminals/fps_terminals/server.py
@@ -13,7 +13,8 @@ def open_terminal(command="bash", columns=80, lines=24):
     pid, fd = pty.fork()
     if pid == 0:
         argv = shlex.split(command)
-        env = dict(TERM="linux", LC_ALL="en_GB.UTF-8", COLUMNS=str(columns), LINES=str(lines))
+        env = os.environ.copy()
+        env.update(TERM="linux", LC_ALL="en_GB.UTF-8", COLUMNS=str(columns), LINES=str(lines))
         os.execvpe(argv[0], argv, env)
     return fd
 
