e15a7e4091cfa94dff41b05cf273ed92438ad299
David Brochart <david.brochart@gmail.com>
2023-07-05 10:18:06 +0200

    Add GitHub authentication with fps-auth-fief (#324)
    * Add GitHub authentication with fps-auth-fief
    
    * Allow auth callback to be set from config

--- plugins/auth_fief/fps_auth_fief/config.py
+++ plugins/auth_fief/fps_auth_fief/config.py
@@ -1,7 +1,11 @@
 from jupyverse_api.auth import AuthConfig
+from pydantic import Field
 
 
 class _AuthFiefConfig(AuthConfig):
-    base_url: str  # Base URL of Fief tenant
-    client_id: str  # ID of Fief client
-    client_secret: str  # Secret of Fief client
+    base_url: str = Field(description="Base URL of Fief tenant")
+    callback_url: str = Field(description="URL of the callback route", default="")
+    client_id: str = Field(description="ID of Fief client")
+    client_secret: str = Field(description="Secret of Fief client")
+    admin_api_key: str = Field(description="Admin API key", default="")
+    oauth_provider_id: str = Field(description="OAuth provider ID", default="")
