740cc936c7cdcef7a0e40cd1a7110fa23be49f07
David Brochart <david.brochart@gmail.com>
2023-09-06 15:31:19 +0200

    Remove try/except when launching jupyverse

--- plugins/auth_jupyterhub/fps_auth_jupyterhub/launch.py
+++ plugins/auth_jupyterhub/fps_auth_jupyterhub/launch.py
@@ -7,16 +7,13 @@ from jupyverse_api.cli import main
 def launch():
     service_url = unquote(os.environ.get("JUPYTERHUB_SERVICE_URL"))
     url = urlparse(service_url)
-    try:
-        return main.callback(
-            open_browser=True,
-            host=url.hostname,
-            port=url.port,
-            set_=(
-                f"frontend.base_url={url.path}",
-                f"app.mount_path={url.path}",
-            ),
-            disable=[],
-        )
-    except Exception:
-        return
+    return main.callback(
+        open_browser=True,
+        host=url.hostname,
+        port=url.port,
+        set_=(
+            f"frontend.base_url={url.path}",
+            f"app.mount_path={url.path}",
+        ),
+        disable=[],
+    )
