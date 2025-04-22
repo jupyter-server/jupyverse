2bf2dc8fd8ed1e92b618e2c8406236fb32b2f38d
David Brochart <david.brochart@gmail.com>
2025-04-22 13:47:52 +0200

    Remove fps_yjs/ydocs

--- plugins/yjs/fps_yjs/ydocs/__init__.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-if sys.version_info < (3, 10):
-    from importlib_metadata import entry_points
-else:
-    from importlib.metadata import entry_points
-
-ydocs = {ep.name: ep.load() for ep in entry_points(group="jupyter_ydoc")}
