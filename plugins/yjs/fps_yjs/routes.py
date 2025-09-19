14afa5735b08622b90a99b5bb4fa2527e869147d
apeng-stripe <apeng@stripe.com>
2025-09-19 03:26:16 -0400

    Filter SYNC_STEP2 messages from read-only clients (#546)
    * fix read-only bypass
    
    * Pin click <8.3.0
    
    * Again
    
    ---------
    
    Co-authored-by: David Brochart <david.brochart@gmail.com>

--- plugins/yjs/fps_yjs/routes.py
+++ plugins/yjs/fps_yjs/routes.py
@@ -302,7 +302,10 @@ class RoomManager:
             # skip = True
             pass
         elif byte == YMessageType.SYNC:
-            if not can_write and msg[0] == YSyncMessageType.SYNC_UPDATE:
+            if not can_write and msg[0] in {
+                YSyncMessageType.SYNC_UPDATE,
+                YSyncMessageType.SYNC_STEP2,
+            }:
                 skip = True
         else:
             skip = True
