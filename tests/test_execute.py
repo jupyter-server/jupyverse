f04f30b1936610e28954a8745dcf0b8edb0bcb31
David Brochart <david.brochart@gmail.com>
2025-11-14 14:22:22 +0100

    Again

--- tests/test_execute.py
+++ tests/test_execute.py
@@ -193,7 +193,7 @@ async def connect_ywidget(ws_url, guid):
         model_name = Text()
         ywidget_doc["_attrs"] = attrs
         ywidget_doc["_model_name"] = model_name
-        with anyio.fail_after(1):
+        with anyio.fail_after(2):
             while True:
                 await anyio.sleep(0.1)
                 if str(model_name) == "Switch" and str(attrs) == '{"value":true}':
